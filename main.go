a5c127cae052f226aa23dc78fcf1217d187884d9
Bazinga <maohua.ethan.wang@gmail.com>
2020-10-19 17:52:55 -0500

    format

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func retrieve(host string, path string, buf *bytes.Buffer) bool{
                         "Connection: keep-alive\r\n" + 
                         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" +
                         "Accept-Encoding: gzip,deflate,br\r\n" + 
-                      "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15\r\n" + 
+                        "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15\r\n" + 
                         "\r\n\r\n")
 
     if conn.SetReadDeadline(time.Now().Add(1 * time.Second)) != nil {
